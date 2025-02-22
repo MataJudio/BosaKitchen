471c060b2e288270427963088ef31162ba3b9307
asen.lucky@yahoo.it <asenlucky9@gmail.com>
2025-02-22 17:54:25 -0500

    Initial commit for BosaKitchen

---
+++ BosaKitchen/src/main/java/com/BosaKitchen/BosaKitchenApplication.java
@@ -0,0 +1,13 @@
+package com.BosaKitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BosaKitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BosaKitchenApplication.class, args);
+	}
+
+}
