5cae9ff07a7db3bc87b55b6e7d1e30bf41c27a79
lucky asen <asenlucky9@gmail.com>
2025-03-13 13:06:13 -0400

    Delete BosaKitchen directory

--- BosaKitchen/src/main/java/com/BosaKitchen/repository/DeliveryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.BosaKitchen.repository;
-
-import com.BosaKitchen.models.DeliveryModels;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeliveryRepository extends JpaRepository<DeliveryModels, Long> {
-}
--- BosaKitchen/src/main/java/com/BosaKitchen/repository/RestaurantDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.BosaKitchen.repository;
-
-import com.BosaKitchen.models.RestaurantDetailModels;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RestaurantDetailRepository extends JpaRepository<RestaurantDetailModels, Long> {
-}
